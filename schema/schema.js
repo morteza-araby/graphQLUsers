f55dd943071c2d42e367f4d096949647a3721a66
Morteza <morteza.araby@outlook.com>
2017-05-27 21:13:19 +0200

    Add company to schema to be able to query directly

--- schema/schema.js
+++ schema/schema.js
@@ -46,6 +46,14 @@ const RootQuery = new GraphQLObjectType({
 						return resp.data;
 					});
 			}
+		},
+		company: {
+			type: CompanyType,
+			args: {id: {type: GraphQLString}},
+			resolve(parentValue, args) {
+				return axios.get(`http://localhost:3000/companies/${args.id}`)
+					.then(resp => resp.data);
+			}
 		}
 	}
 });
