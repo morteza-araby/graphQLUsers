62cb0401256355d79f2160d7349e9117f609eaa8
Morteza <morteza.araby@outlook.com>
2017-05-29 11:18:11 +0200

    Added deleteUser mutation can be used like: mutation {  deleteUser(id: "SkZYCItWZ") {     id   } }

--- schema/schema.js
+++ schema/schema.js
@@ -81,7 +81,18 @@ const mutation = new GraphQLObjectType({
 				return axios.post('http://localhost:3000/users/', { firstName, age })
 					.then(resp => resp.data);
 			}
-		}
+		},
+
+		deleteUser: {
+			type: UserType,
+			args: {
+				id: { type: new GraphQLNonNull(GraphQLString) },
+			},
+			resolve(parentValue, args) {
+				return axios.delete(`http://localhost:3000/users/${args.id}`)
+					.then(resp => resp.data)
+			}
+		},		
 	}
 });
 
